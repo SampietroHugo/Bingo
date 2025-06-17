647c36a5f15452a5a3a554278ed20425135f5d6c
SampietroHugo <victorhsampietro2@gmail.com>
2025-06-17 14:54:11 -0300

    Aprendendo/testando matriz

--- src/bingo.java
+++ src/bingo.java
@@ -3,6 +3,20 @@ import java.util.*;
 public class bingo{
     public static Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
-        
+        Random random = new Random();
+        int [][] cartela = new int [5][5];
+
+        for (int i = 0; i < 5; i++){
+            for (int j = 0; j < 5; j++){
+                cartela[i][j] = i + j;
+            }
+        }
+        for (int i = 0; i < 5; i++){
+            for (int j = 0; j < 5; j++){
+                System.out.print(cartela[i][j] + " ");
+            }
+            System.out.println();
+        }
+
     }
 }
