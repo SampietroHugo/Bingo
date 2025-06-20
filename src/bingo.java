98de086f024c281c9d5f15fef15577d3e0426b7a
SampietroHugo <victorhsampietro2@gmail.com>
2025-06-20 00:24:17 -0300

    Bingo 2.1 Bugs corrigidos

--- src/bingo.java
+++ src/bingo.java
@@ -7,51 +7,61 @@ public class bingo {
         int resposta;
         Random random = new Random();
         int[][] cartela = new int[3][3];
-
-        for (int i = 0; i < cartela.length; i++) {
-            for (int j = 0; j < cartela.length; j++) {
-                cartela[i][j] = random.nextInt(10, 50);
-            }
-        }
+        int[][] cartelaOculta = new int[3][3];
 
         for (int i = 0; i < cartela.length; i++) {
             for (int j = 0; j < cartela[i].length; j++) {
-                do {
-                    System.out.println();
-                    System.out.print("Digite um número de 10 a 50 para o bingo: ");
-                    resposta = input.nextInt();
-                } while (resposta < 10 || resposta > 99);
-
-                boolean acertou = false;
-
-                for (int x = 0; x < cartela.length; x++) {
-                    for (int y = 0; y < cartela[x].length; y++) {
-                        if (resposta == cartela[x][y]) {
-                            System.out.println("Acertou! \nLinha: " + (x + 1) + " \nColuna: " + (y + 1));
-                            cartela[x][y] = 0;
-                            acertou = true;
-                        }
-                    }
-                }
-                if (!acertou) {
-                    System.out.println("Número não encontrado na cartela.");
-
-                }
-
-                System.out.println("\nCartela atualizada:");
-                for (int x = 0; x < cartela.length; x++) {
-                    for (int y = 0; y < cartela[x].length; y++) {
-                        if(cartela[x][y] == 0){
-                            System.out.print(resposta + "\t");
-                        }else{
-                            System.out.print( "X\t");
-                        }
-                    }
-                    System.out.println();
-                }
-
+                cartela[i][j] = random.nextInt(10, 51);
             }
         }
-        System.out.println("\nFim do Bingo!!");
+
+        while (!cartelaCompleta(cartela)) {
+            do {
+                System.out.print("\nDigite um número de 10 a 50 para o bingo: ");
+                resposta = input.nextInt();
+            } while (resposta < 10 || resposta > 50);
+
+            boolean acertou = false;
+
+            for (int i = 0; i < cartela.length; i++) {
+                for (int j = 0; j < cartela[i].length; j++) {
+                    if (resposta == cartela[i][j]) {
+                        System.out.println("Acertou! Linha: " + (i + 1) + " | Coluna: " + (j + 1));
+                        cartelaOculta[i][j] = cartela[i][j]; 
+                        cartela[i][j] = 0; 
+                        acertou = true;
+                    }
+                }
+            }
+
+            if (!acertou) {
+                System.out.println("Número não encontrado na cartela.");
+            }
+
+            System.out.println("\nCartela atualizada:");
+            for (int i = 0; i < cartelaOculta.length; i++) {
+                for (int j = 0; j < cartelaOculta[i].length; j++) {
+                    if (cartelaOculta[i][j] != 0) {
+                        System.out.print(cartelaOculta[i][j] + "\t");
+                    } else {
+                        System.out.print("X\t");
+                    }
+                }
+                System.out.println();
+            }
+        }
+
+        System.out.println("\nBINGO! Você acertou todos os números!");
+    }
+
+    public static boolean cartelaCompleta(int[][] cartela) {
+        for (int i = 0; i < cartela.length; i++) {
+            for (int j = 0; j < cartela[i].length; j++) {
+                if (cartela[i][j] != 0) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
